42042e5b7d826ac8ff42c11cf47b4fdab208a71e
anton-patrushev <anton11patrushev@gmail.com>
2022-03-14 02:19:25 +0300

    enable decorators

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
     [
       require.resolve('babel-plugin-module-resolver'),
       {
