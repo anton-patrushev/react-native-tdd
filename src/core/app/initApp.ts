5e5319533a36259a37e0e4c6a92828c8cd2adb64
Anton Patrushev <62186274+anton-patrushev@users.noreply.github.com>
2022-04-24 18:13:10 +0300

    retest merge-it bot (#7)
    * bla bla bla comment
    
    * Update initApp.ts
    
    * Empty commit from the merge-bot 👋
    
    * Empty commit from the merge-bot 👋
    
    Co-authored-by: mergeit-bot[bot] <104276220+mergeit-bot[bot]@users.noreply.github.com>

--- src/core/app/initApp.ts
+++ src/core/app/initApp.ts
@@ -4,6 +4,8 @@ import App from 'src/core/app/App';
 import { initializeDIContainer } from 'src/core/ioc/container/init';
 
 export const appRegister = () => App;
+// bla-bla-bla for testing
+// bla-bla-bla for testing 2
 
 export default async function initApp(appName: string) {
   await initializeDIContainer();
