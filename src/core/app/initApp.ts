f1b294a88629434a99c971c3999032e8ab2102c3
Anton Patrushev <62186274+anton-patrushev@users.noreply.github.com>
2022-04-24 18:06:59 +0300

    bla bla bla comment

--- src/core/app/initApp.ts
+++ src/core/app/initApp.ts
@@ -4,6 +4,7 @@ import App from 'src/core/app/App';
 import { initializeDIContainer } from 'src/core/ioc/container/init';
 
 export const appRegister = () => App;
+// bla-bla-bla for testing
 
 export default async function initApp(appName: string) {
   await initializeDIContainer();
